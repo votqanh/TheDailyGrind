06278ca0bd3fc2bee7c10f6cea1e5b3facc52227
nazie23 <edroos.nazia@gmail.com>
2025-03-08 14:26:10 -0800

    created linkedin scraper for posts (rn just scrapes posts by microsoft)

--- backend/app.py
+++ backend/app.py
@@ -24,8 +24,8 @@ def generate_content():
     # Return the response text as JSON
     return jsonify({'response': response.text})
 
-@app.route('/linkedin')
-def linkedin_data():
+@app.route('/linkedin-profile')
+def linkedin_profile():
     url = "https://linkedin-data-api.p.rapidapi.com/"
 
     querystring = {"username":"nazia-edroos-593b49217"}
@@ -36,9 +36,36 @@ def linkedin_data():
     }
 
     response = requests.get(url, headers=headers, params=querystring)
+    return jsonify(response.json())
 
+@app.route('/linkedin-posts')
+def linkedin_posts():
+    url = "https://linkedin-data-api.p.rapidapi.com/search-posts"
+
+    payload = {
+        "keyword": "microsoft",
+        "sortBy": "date_posted",
+        "datePosted": "",
+        "page": 1,
+        "contentType": "",
+        "fromMember": ["ACoAAAEkwwAB9KEc2TrQgOLEQ-vzRyZeCDyc6DQ", "ACoAAANuWM8BtmA18VYdgqPtIWt6GhBCTDXToV4", "ACoAAA8BYqEBCGLg_vT_ca6mMEqkpp9nVffJ3hc"],
+        "fromCompany": [1441, 1035],
+        "mentionsMember": ["ACoAAAEkwwAB9KEc2TrQgOLEQ-vzRyZeCDyc6DQ", "ACoAAA8BYqEBCGLg_vT_ca6mMEqkpp9nVffJ3hc"],
+        "mentionsOrganization": [1441, 1035],
+        "authorIndustry": [96, 4],
+        "authorCompany": [1035],
+        "authorTitle": ""
+    }
+    headers = {
+        "x-rapidapi-key": "31f89230e0mshc68458479372f61p16a3c3jsna0d63ef48498",
+        "x-rapidapi-host": "linkedin-data-api.p.rapidapi.com",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
     return jsonify(response.json())
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
