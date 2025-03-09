6dd84a6a85247c693562a7376397e468458b9d41
nazie23 <edroos.nazia@gmail.com>
2025-03-08 16:03:27 -0800

    minor fix to README.md and app.py

--- backend/app.py
+++ backend/app.py
@@ -12,18 +12,6 @@ genai.configure(api_key="AIzaSyCJHST-dJuFixWFYmq2LsbGl7A_tzAFYss")
 def home():
     return jsonify({'message': 'Hello from Flask!'})
 
-# @app.route('/generate')
-# def generate_content():
-#     # Generate content using Google Generative AI
-#     model = genai.GenerativeModel("gemini-2.0-flash")
-#     response = model.generate_content("Explain how AI works")
-    
-#     # Print the response text to the console
-#     print(response.text)
-    
-#     # Return the response text as JSON
-#     return jsonify({'response': response.text})
-
 @app.route('/generate-summary')
 def linkedin_profile():
     url = "https://linkedin-data-api.p.rapidapi.com/"
